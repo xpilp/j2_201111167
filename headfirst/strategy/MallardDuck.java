5246133477e299721adc9c655460adc8d144d772
xpilp <leechwon92@gmail.com>
2017-03-22 14:52:49 +0900

    Add files via upload

--- headfirst/strategy/MallardDuck.java
+++ headfirst/strategy/MallardDuck.java
@@ -3,9 +3,8 @@ package headfirst.strategy;
 public class MallardDuck extends Duck {
  
  public MallardDuck() {
- 
-  quackBehavior = new Quack();
-        flyBehavior = new FlyWithWings();
+   setFlyBehavior (new FlyWithWings()) ;
+   setQuackBehavior (new Quack());
  }
  
  public void display() {
--- headfirst/strategy/ModelDuck.java
+++ headfirst/strategy/ModelDuck.java
@@ -2,8 +2,8 @@ package headfirst.strategy;
 
 public class ModelDuck extends Duck {
  public ModelDuck() {
-  flyBehavior = new FlyNoWay();
-  quackBehavior = new Quack();
+  setFlyBehavior (new FlyWithWings());
+  setQuackBehavior( new Quack());
  }
 
  public void display() {
