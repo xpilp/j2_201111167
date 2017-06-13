707ee0554972ad52ad36801fb77173ef334f75ae
201111167 이철원 <leechwon92@gmail.com>
2017-06-13 13:40:19 +0900

    Add files via upload

---
+++ headfirst/state/gumballstate/State.java
@@ -0,0 +1,8 @@
+package headfirst.state.gumballstate;
+
+public interface State {
+  public void insertQuarter();
+  public void ejectQuarter();
+  public void turnCrank();
+  public void dispense();
+}
